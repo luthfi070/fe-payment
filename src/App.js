1166e05f725c6e7d7437896214fe6286bbd538a5
luthfi070 <luthfimusafa@gmail.com>
2023-06-09 20:38:34 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // You can also change below url value to any script url you wish to load,
+    // for example this is snap.js for Sandbox Env (Note: remove `.sandbox` from url if you want to use production version)
+    const midtransScriptUrl = "https://app.sandbox.midtrans.com/snap/snap.js";
+
+    let scriptTag = document.createElement("script");
+    scriptTag.src = midtransScriptUrl;
+
+    // Optional: set script attribute, for example snap.js have data-client-key attribute
+    // (change the value according to your client-key)
+    const myMidtransClientKey = "";
+    scriptTag.setAttribute("data-client-key", myMidtransClientKey);
+
+    document.body.appendChild(scriptTag);
+
+    return () => {
+      document.body.removeChild(scriptTag);
+    };
+  }, []);
+
+  const bayar = () => {
+    axios
+      .post("http://localhost:3000/api/siswa/payment", {
+        harga: 40000,
+      })
+      .then((res) => {
+        // console.log(res.data.token);
+
+        window.snap.pay(res.data.token);
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.asdasd
         </p>
+        <button onClick={() => bayar()}>Bayar</button>
         <a
           className="App-link"
           href="https://reactjs.org"
